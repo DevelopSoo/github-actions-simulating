b4716f16464dff563f67c7ca38e81ac970d5fff6
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-05-10 13:08:32 +0900

    환경변수 추가2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 // src/app/page.tsx
 
 export default function Home() {
-  return <h1>게시글 목록4</h1>;
+  return (
+    <div>
+      <h1>게시글 목록4</h1>
+      <p>환경: {process.env.NEXT_PUBLIC_MY_ENVIRONMENT}</p>
+    </div>
+  );
 }

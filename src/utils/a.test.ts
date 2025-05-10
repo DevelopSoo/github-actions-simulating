14f84e1bc12dc7277d7154044fc16c044d18854e
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-05-10 18:37:43 +0900

    새로운 테스트 만들기

---
+++ src/utils/a.test.ts
@@ -0,0 +1,5 @@
+import { a } from "./a";
+
+test("a", () => {
+  expect(a(3)).toBe(2);
+});
---
+++ src/utils/a.ts
@@ -0,0 +1,11 @@
+export function a(a: number) {
+  if (a === 2) {
+    return 1;
+  }
+
+  if (a === 3) {
+    return 2;
+  }
+
+  return 3;
+}

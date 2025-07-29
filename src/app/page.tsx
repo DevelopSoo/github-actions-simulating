c5cbb88235e2decb244b873957a13df7d31eb150
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-07-29 19:01:02 +0900

    캐시여부 확인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="flex h-screen items-center justify-center">
       <div className="w-80">
         <Input isError={true} errorMessage="입력값에 문제가 있습니다" />
+        <div>hi</div>
       </div>
     </div>
   );
